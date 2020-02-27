671d7324d915a7e10eddf7dc09059ec41a7ddc39
Megan Horsfield <meganthadev@gmail.com>
2020-02-27 05:28:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,15 @@ class Artist
   end
 end
 
-  def self.song_count
-    @@song_count
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+      artist_count[artist] +=1
+      else
+      song_count[artist] = 1
+      end
+    end
+    song_count
   end
 end
